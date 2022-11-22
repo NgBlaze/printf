a09533d19ebb6b9dc2083ef43ef38eda1c8f78a9
osa231 <osa_agbonwanegbe@yahoo.com>
2022-11-22 10:15:35 -0800

    getprnt

--- get_print.c
+++ get_print.c
@@ -27,7 +27,7 @@ int (*get_print(char s))(va_list, flags_t *)
 		{'r', print_rev},
 		{'S', print_bigS},
 		{'p', print_address},
-		{'%', print_percent}
+		{'%', print_percentage}
 		};
 	int flags = 14;
 
