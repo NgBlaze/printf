1c13b12f2024edc95905ce8085865fc7a64d40a8
NgBlaze <okolaaprecious@gmail.com>
2022-11-22 10:01:47 -0800

    updated func

--- print_nums.c
+++ print_nums.c
@@ -69,5 +69,17 @@ void print_number(int n)
  */
 int count_digit(int i)                                                                                                                                                                        
 {                                                                                                                                                                                             
-	unsigned int d = 0;                                                                                                                                                                   
-	unsigned int u;          
+  unsigned int d = 0;                                                                                                                                                                   
+  unsigned int u;                                                                                                                                                                       
+                                                                                                                                                                                              
+  if (i < 0)                                                                                                                                                                            
+    u = i * -1;                                                                                                                                                                   
+  else                                                                                                                                                                                  
+    u = i;                                                                                                                                                                        
+  while (u != 0)                                                                                                                                                                        
+    {                                                                                                                                                                                     
+      u /= 10;                                                                                                                                                                      
+      d++;                                                                                                                                                                          
+    }                                                                                                                                                                                     
+  return (d);                                                                                                                                                                           
+}
